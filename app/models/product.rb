e26fe8b79b7c569f23daea3ff35c4bad7437192b
Backbencher Technologies <95627590+backbenchertechnologies@users.noreply.github.com>
2022-12-28 15:46:09 +0530

    product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
     belongs_to :company
+    
+     validates :name, presence: true 
+
+    validates :description, presence: true 
+
+    validates :category,  presence: true 
 end
