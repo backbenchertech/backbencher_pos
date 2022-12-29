b9c1a8b10bceff17671c41576cbcca632135b4d0
Harshit <bhawsarharshit@gmail.com>
2022-12-29 14:36:30 +0530

    generated new model Category

--- db/migrate/20221227153946_create_products.rb
+++ db/migrate/20221227153946_create_products.rb
@@ -3,7 +3,6 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.string :name
       t.string :description
-      t.string :category
       t.integer :company_id
 
       t.timestamps
