4d2d10840d3baab03dc0536492e53fced4fe2f51
Sourabh gurjar <sourabh.gurjar@backbenchertechnologies.com>
2022-12-29 20:36:39 +0530

    Last Changes

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,2 +1,8 @@
 class ProductType < ApplicationRecord
+    serialize :product_types, JSON
+    attr_accessor :service, :real, :virtual
+
+    def attr_accessor
+        {'service' => nil, 'real' => nil, 'virtual' => nil}
+    end
 end
