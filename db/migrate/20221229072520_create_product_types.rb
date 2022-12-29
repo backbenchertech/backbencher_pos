f2ded1e40a9257dccead1c4aca2b25046f90cbcb
Sourabh gurjar <sourabh.gurjar@backbenchertechnologies.com>
2022-12-29 14:02:38 +0530

    added new type table

---
+++ db/migrate/20221229072520_create_product_types.rb
@@ -0,0 +1,13 @@
+class CreateProductTypes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :product_types do |t|
+      t.string :name
+      t.boolean :has_quantity
+      t.boolean :is_default
+      t.integer :product_id
+    
+
+      t.timestamps
+    end
+  end
+end
