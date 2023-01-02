f776cea837c2e9c51449758b709937b21a3ccfc4
tanyatanu285 <ts5487968@gmail.com>
2023-01-02 15:21:40 +0530

    added some changes for the indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-    # Association
+  # Association
+  has_many :addresses, as: :addressable
 
-    has_many :addresses, as: :addressable
-
-    #validation 
-   
-    validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i } , uniqueness: true
-    validates :phone_no, presence: true, format: { with: /\A\d{10}\z/ }
+  #validation
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i } , uniqueness: true
+  validates :phone_no, presence: true, format: { with: /\A\d{10}\z/ }
 end
